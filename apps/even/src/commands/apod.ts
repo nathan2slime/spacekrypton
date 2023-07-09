77db9b9812c4f0ab4d04f22ecad4c95722fb2703
Jhonathan <nathan3boss@gmail.com>
2023-07-09 05:25:38 -0300

    feat(even): slash commands (#3)

--- apps/even/src/commands/apod.ts
+++ apps/even/src/commands/apod.ts
@@ -3,15 +3,76 @@ import {
   CommandInteraction,
   EmbedBuilder,
 } from 'discord.js';
-import { Discord, Slash, SlashOption } from 'discordx';
-import { format, parse, toDate } from 'date-fns';
+import {
+  Discord,
+  SimpleCommand,
+  SimpleCommandMessage,
+  SimpleCommandOption,
+  SimpleCommandOptionType,
+  Slash,
+  SlashOption,
+} from 'discordx';
+import { format, parse } from 'date-fns';
 
 import { getTodayApod } from '../services/apod';
 
 @Discord()
 export class Apod {
+  async command(
+    value: string,
+    simple?: SimpleCommandMessage,
+    slash?: CommandInteraction
+  ) {
+    const getDate = () => new Date(parse(value, 'dd/MM/yyyy', new Date()));
+
+    if (value) {
+      try {
+        format(getDate(), 'yyyy-MM-dd');
+      } catch (error) {
+        const reply =
+          'Choose dates from June 16, 1995 and in the format 08/03/2003';
+        return slash ? slash.reply(reply) : simple?.message.reply(reply);
+      }
+    }
+
+    const data = await getTodayApod(
+      format(value ? getDate() : new Date(), 'yyyy-MM-dd')
+    );
+
+    if (data) {
+      const isImage = data.media_type == 'image';
+      const embed = new EmbedBuilder({
+        title: data.title,
+        description: `${data.explanation}\n\nDate: ${format(
+          new Date(data.date),
+          'dd/MM/yyyy'
+        )}`,
+      });
+
+      data.media_type == 'image'
+        ? embed.setImage(data.url)
+        : embed.setFields([
+            {
+              name: 'Media',
+              value: data.url,
+            },
+          ]);
+
+      slash
+        ? slash.reply({
+            embeds: [embed],
+          })
+        : simple?.message.reply({
+            embeds: [embed],
+          });
+    } else {
+      const reply = "I couldn't find an apod for that day";
+      slash ? slash.reply(reply) : simple?.message.reply(reply);
+    }
+  }
+
   @Slash({ description: 'Returns astronomy picture day', name: 'apod' })
-  async apod(
+  async slashCommandApod(
     @SlashOption({
       description: 'Photo date (03/08/2003)',
       name: 'date',
@@ -21,39 +82,15 @@ export class Apod {
     value: string,
     interaction: CommandInteraction
   ) {
-    const getDate = () => new Date(parse(value, 'dd/MM/yyyy', new Date()));
+    await this.command(value, undefined, interaction);
+  }
 
-    if (value) {
-      try {
-        format(getDate(), 'yyyy-MM-dd');
-      } catch (error) {
-        return interaction.reply(
-          'Choose dates from June 16, 1995 and in the format 08/03/2003'
-        );
-      }
-    }
-
-    const data = await getTodayApod(
-      format(value ? getDate() : new Date(), 'yyyy-MM-dd')
-    );
-
-    if (data) {
-      const embed = new EmbedBuilder({
-        title: data.title,
-        description: `${data.explanation}\n\nDate: ${format(
-          new Date(data.date),
-          'dd/MM/yyyy'
-        )}`,
-        image: {
-          url: data.url,
-        },
-      });
-
-      interaction.reply({
-        embeds: [embed],
-      });
-    } else {
-      interaction.reply("I couldn't find an apod for that day");
-    }
+  @SimpleCommand({ description: 'Returns astronomy picture day', name: 'apod' })
+  async simpleCommandApod(
+    @SimpleCommandOption({ name: 'name', type: SimpleCommandOptionType.String })
+    value: string,
+    interaction: SimpleCommandMessage
+  ) {
+    await this.command(value, interaction);
   }
 }
